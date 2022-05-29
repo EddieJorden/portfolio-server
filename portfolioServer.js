043814fdfc4052b7d1e3ed4cc0f5211e0d79ac68
EddieJorden <eddie@eddiejorden.tech>
2022-05-28 18:15:48 -0700

    added get and post methods for user authentication

--- portfolioServer.js
+++ portfolioServer.js
@@ -6,6 +6,21 @@ const api = express();
 
 const data = require('./sampleData.json');
 
+let authorizedUsers = [{name: 'eddie'}]
+
+let token = null
+
+let getToken = () => {
+	token = Math.floor(Math.random(9999999) * 10000000)
+	let timeout = 1000 * 60
+	console.log(token)
+	setTimeout(() => {
+		token = null
+		console.log(token)
+	}, timeout)
+	return token
+}
+
 let item = []
 
 api.use(express.json())
@@ -21,6 +36,24 @@ api.get('/', (req, res) => {
 api.get('/data', (req, res) => {
 	res.status(200).json(data)
 });
+
+api.get('/getUserToken', (req, res) => {
+	if(authorizedUsers.findIndex(user => user.name == req.body.name) != -1) {
+		if(token === null) {
+			token = getToken()
+		} res.status(200).json(token)
+	} res.status(404).json('user not found')
+	}
+)
+
+api.post('/addUser', (req, res) => {
+	if(authorizedUsers.findIndex(user => user.name == req.body.name) == -1 && authorizedUsers.findIndex(user => user.email == req.body.email) == -1) {
+		authorizedUsers = [...authorizedUsers, {name: req.body.name, email: req.body.email}]
+		console.log('user added ', authorizedUsers)
+	}
+	console.log('user already exists', authorizedUsers)
+		res.status(202).send('user added')
+})
 
 api.post('/addItem', (req, res) =>  {
 	console.log('new post!', req.body)
