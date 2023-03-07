53f8755fca26e4f03943ee57abf6d5a01ae78083
EddieJorden <eddie@eddiejorden.tech>
2023-03-07 08:16:31 -0800

    added gitignore and ignored nodemodules

---
+++ myProjects.js
@@ -0,0 +1,166 @@
+/* eslint-disable react/react-in-jsx-scope */
+const myProjectsArray = [
+	{
+		name: 'Full Stack Task Manager App',
+		thumbnail: 'Task-app-thumbnail.png',
+		altText: 'Full Stack Task Manager App',
+		link: 'https://eddies-balance-app.netlify.app/',
+		linkText: 'Full Stack Task Manager App',
+		notes: {
+			notes:
+				'This is a full stack task manager app built using React, Redux, Typescript, Node.js, Express.js, and MySQL.',
+		},
+		github: 'https://github.com/EddieJorden/balance-app',
+	},
+	{
+		name: 'Custom AI chat bot',
+		thumbnail: 'AI-chatbot-thumbnail.png',
+		altText: 'custom AI chatbot',
+		link: 'https://eddies-balance-app.netlify.app/',
+		linkText: 'custom AI chatbot',
+		notes: {
+			notes:
+				'Here is a custom AI chat bot that can answer questions and produce text.',
+		},
+		github: 'https://github.com/EddieJorden/balance-app',
+	},
+	{
+		name: 'SimpleSpectra.com',
+		thumbnail: 'SimpleSpectra.com-thumbnail.png',
+		altText: 'SimpleSpectra.com thumbnail',
+		link: 'https://testspectra.wpengine.com/',
+		linkText: 'SimpleSpectra.com',
+		notes: {
+			notes: 'E-commerce site(currently in development)',
+		},
+	},
+	{
+		name: 'Data Visualization',
+		thumbnail: 'Data Visualization with D3 thumbnail.png',
+		altText: 'Data Visualization with D3 thumbnail',
+		link: 'https://eddiejorden.github.io/ui-ux-project/',
+		linkText: 'Data Visualization',
+		notes: {
+			notes:
+				'A data visualization component built using D3.js with React, Redux and Express.js.',
+		},
+		github: 'https://github.com/EddieJorden/ui-ux-project',
+	},
+	{
+		name: 'Editable Fields With Filter',
+		thumbnail: 'React-Reudx-Challenge-thumbnail.png',
+		altText: 'React-Redux-Challenge-project',
+		link: 'https://eddiejorden.github.io/ctw-project/',
+		linkText: 'React Redux Challenge Project',
+		notes: {
+			notes:
+				'A React Redux application,  that renders 1000 input fields, all with the ability to update the state, as well as filters.',
+		},
+		github: 'https://github.com/EddieJorden/ctw-project',
+	},
+	{
+		name: 'Reddit Clone',
+		thumbnail: 'reddit_clone_thumbnail.png',
+		altText: 'reddit-clone',
+		link: 'https://eddiejorden.github.io/reddit-clone/',
+		linkText: 'Reddit Clone',
+		notes: {
+			notes: 'A Simple Reddit Clone using reddit API in a React, Redux app.',
+		},
+		github: 'https://github.com/EddieJorden/reddit-clone',
+	},
+	{
+		name: 'Flash Card Tests',
+		thumbnail: 'flashcards-thumbnail.png',
+		altText: 'alt text for thumbnail here',
+		link: 'https://eddiejorden.github.io/flashcards/',
+		linkText: 'Flash Cards',
+		notes: {
+			notes:
+				'This custom Flash Cards app was built using React with Redux. It allows you to make flashcard quizzes for topics you decide,  you can name the quizzes and add custom created flashcards.',
+		},
+		github: 'https://github.com/EddieJorden/flashcards',
+	},
+	{
+		name: 'Fotomatic',
+		thumbnail: 'fotomatic-thumbnail.png',
+		altText: 'fotomatic-thumbnail',
+		link: 'https://eddiejorden.github.io/fotomatic/',
+		linkText: 'Fotomatic',
+		notes: {
+			notes:
+				'Responsive CSS using flexbox created to exact designer specifications',
+		},
+		github: 'https://github.com/EddieJorden/fotomatic',
+	},
+	{
+		name: 'My Planner',
+		thumbnail: 'appointment-planner-thumbnail.png',
+		altText: 'appointment-planner-thumbnail',
+		link: 'https://eddiejorden.github.io/react_challenge/',
+		linkText: 'My Planner',
+		notes: {
+			notes: 'Contacts and appointments component built using React.',
+		},
+		github: 'https://github.com/EddieJorden/react_challenge/settings/pages',
+	},
+	{
+		name: 'Food Near Me',
+		thumbnail: 'food_near_me_thumbnail.png',
+		altText: 'food-near-me-thumbnail',
+		link: 'https://eddiejorden.github.io/food.near.me/',
+		linkText: 'Food Near Me',
+		notes: {
+			notes:
+				'This is a React app built using the Yelp api, since it is in developent you must first enable cors anywhere before using this app,  click the link below and click "request temporary access to the demo server" then return to the app.',
+			notesLink: (
+				<a
+					href="https://cors-anywhere.herokuapp.com/corsdemo"
+					target="_blank"
+					rel="noopener noreferrer"
+					title="link to cors anywhere">
+					cors anywhere
+				</a>
+			),
+		},
+		github: 'https://github.com/EddieJorden/food.near.me',
+	},
+	{
+		name: 'My Jams',
+		thumbnail: 'myJams-thumbnail.png',
+		altText: 'my-jams-thumbnail',
+		link: 'https://eddiejorden.github.io/my-jams',
+		linkText: 'My Jams',
+		notes: {
+			notes:
+				'Using the Spotify api I have built this React app to allow me to create playlists, and add tracks to existing playslists. Please login with spotify username: "test@eddiemoger.com" password: "testpassword" to use this app.',
+		},
+		github: 'https://github.com/EddieJorden/my-jams',
+	},
+	{
+		name: 'This Portfolio',
+		thumbnail: 'My_Portfolio_thumbnail.png',
+		altText: 'My_Portfolio_thumbnail',
+		link: 'https://eddiemoger.com',
+		linkText: 'This Portfolio',
+		notes: {
+			notes:
+				'This responsive portfolio was built from scratch using React.js and Express.',
+		},
+		github: 'https://github.com/EddieJorden/my-portfolio',
+	},
+
+	// {
+	//     name: 'name of app here',
+	//     thumbnail: 'thumbnail link here',
+	//     altText: 'alt text for thumbnail here',
+	//     link: 'link to project here',
+	//     linkText: 'project link text here',
+	//     notes: {
+	// notes: 'notes here',
+	// }
+	//     github: 'link to this projects github here',
+	// }
+];
+
+export default myProjectsArray;
