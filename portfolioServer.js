86e37fde4e83c733e76b242922e1c9955a3e81d3
EddieJorden <eddie@eddiejorden.tech>
2023-03-07 08:30:43 -0800

    added crud routes for projects

--- portfolioServer.js
+++ portfolioServer.js
@@ -2,80 +2,113 @@
 /* eslint-disable no-undef */
 const express = require('express');
 
-const api = express();
+const app = express();
 
 const cors = require('cors');
 
-api.use(cors())
+app.use(cors());
 
 const data = require('./sampleData.json');
+const projects = require('./myProjects.json');
 
-let authorizedUsers = [{name: 'eddie'}]
+let authorizedUsers = [{ name: 'eddie' }];
 
-let token = null
+let token = null;
 
 let getToken = () => {
-	token = Math.floor(Math.random(9999999) * 10000000)
-	let timeout = 1000 * 60
-	console.log(token)
+	token = Math.floor(Math.random(9999999) * 10000000);
+	let timeout = 1000 * 60;
+	console.log(token);
 	setTimeout(() => {
-		token = null
-		console.log(token)
-	}, timeout)
-	return token
-}
+		token = null;
+		console.log(token);
+	}, timeout);
+	return token;
+};
 
-let item = []
+let item = [];
 
-api.use(express.json())
-api.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const HOST = '0.0.0.0';
 const PORT = 8888;
 
-
-
-api.get('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.send('Welcome to EddieMoger.com');
 });
 
-api.get('/data', (req, res) => {
-	res.status(200).json(data)
+app.get('/data', (req, res) => {
+	res.status(200).json(data);
 });
 
-api.get('/getUserToken', (req, res) => {
-	if(authorizedUsers.findIndex(user => user.name == req.body.name) != -1) {
-		if(token === null) {
-			token = getToken()
-		} res.status(200).json(token)
-	} res.status(404).json('user not found')
+app.get('/getProjects', (req, res) => {
+	res.status(200).json(projects);
+});
+
+app.post('/addProject', (req, res) => {
+	console.log('new post!', req.body);
+	projects = [...projects, req.body];
+	res.status(201).send(`added Project`);
+});
+
+app.post('/removeProjectByName', (req, res) => {
+	console.log('project removed!');
+	projects = projects.filter((project) => project.name != req.body.name);
+	res.status(201).send(`removed Project`);
+});
+
+app.patch('/updateProjectByName', (req, res) => {
+	console.log('project updated!');
+	projects = projects.map((project) => {
+		if (project.name == req.body.name) {
+			return req.body;
+		}
+		return project;
+	});
+	res.status(201).send(`updated Project`);
+});
+
+app.get('/getUserToken', (req, res) => {
+	if (authorizedUsers.findIndex((user) => user.name == req.body.name) != -1) {
+		if (token === null) {
+			token = getToken();
+		}
+		res.status(200).json(token);
 	}
-)
+	res.status(404).json('user not found');
+});
 
-api.post('/addUser', (req, res) => {
-	if(authorizedUsers.findIndex(user => user.name == req.body.name) == -1 && authorizedUsers.findIndex(user => user.email == req.body.email) == -1) {
-		authorizedUsers = [...authorizedUsers, {name: req.body.name, email: req.body.email}]
-		console.log('user added ', authorizedUsers)
+app.post('/addUser', (req, res) => {
+	if (
+		authorizedUsers.findIndex((user) => user.name == req.body.name) == -1 &&
+		authorizedUsers.findIndex((user) => user.email == req.body.email) == -1
+	) {
+		authorizedUsers = [
+			...authorizedUsers,
+			{ name: req.body.name, email: req.body.email },
+		];
+		console.log('user added ', authorizedUsers);
 	}
-	console.log('user already exists', authorizedUsers)
-		res.status(202).send('user added')
-})
+	console.log('user already exists', authorizedUsers);
+	res.status(202).send('user added');
+});
 
-api.post('/addItem', (req, res) =>  {
-	console.log('new post!', req.body)
-	item = [...item, req.body]
-	res.status(201).send(`added Item`)
-})
+app.post('/addItem', (req, res) => {
+	console.log('new post!', req.body);
+	item = [...item, req.body];
+	res.status(201).send(`added Item`);
+});
 
-api.post('/removeItemByName', (req, res) => {
-	console.log('item removed!')
- const poppedItem = item.pop()
-	console.log('poppedItem', poppedItem)
-	res.status(202).send(`Removed Item`)
-})
+app.post('/removeItemByName', (req, res) => {
+	console.log('item removed!');
+	const poppedItem = item.pop();
+	console.log('poppedItem', poppedItem);
+	res.status(202).send(`Removed Item`);
+});
 
-api.get('/getItem', (req, res) => {
-	res.status(200).json(item)
-})
+app.get('/getItem', (req, res) => {
+	res.status(200).json(item);
+});
 
-api.listen(PORT, () => console.log(`API listening at ${HOST}:${PORT}!`));
+app.listen(PORT, () => console.log(`app listening at ${HOST}:${PORT}!`));
